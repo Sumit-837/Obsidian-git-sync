00f29963d58aeb4b9403756e9253c363351412f8
Sumit Roy <sumitroysocial@gmail.com>
2025-11-28 22:21:08 +0530

    solved

--- Notespace/05 Workbook/CS3101/Tests/PYQ/CT-makup-2024/Q3.c
+++ Notespace/05 Workbook/CS3101/Tests/PYQ/CT-makup-2024/Q3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void invertstr(char *arr, int len) {
-  int temp;
+  char temp;
   for (int i = 0; i < len / 2; i++) {
     temp = arr[len - 1 - i];
     *(arr + len - 1 - i) = arr[i];
@@ -11,14 +11,20 @@ void invertstr(char *arr, int len) {
 
 void decrypt(char *string) {
   int len = 0;
+  int star = 1;
   while (string[len] != '\0') {
     len++;
   }
-  invertstr(string, len / 2);
+  while (string[star - 1] != '*') {
+    star++;
+  }
+
+  invertstr(string, star - 1);
+  invertstr(string + star, len - star);
 }
 
 int main() {
-  char *str = "HAMUUDHABIBI";
+  char str[] = "DOOMAH*IBIBAH";
   decrypt(str);
   printf("%s\n", str);
   return 0;
